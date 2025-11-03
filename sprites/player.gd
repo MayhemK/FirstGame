e16b18c50037b80d82bfe83e2cb6de862218b87e
MayhemK <mayhemk57@gmail.com>
2025-11-02 20:57:37 -0700

    adding short hop and long jump, but keeping it shorter than 3 blox tall

--- sprites/player.gd
+++ sprites/player.gd
@@ -3,7 +3,8 @@ extends CharacterBody2D
 
 
 const SPEED = 100.0
-const JUMP_VELOCITY = -250.0
+const JUMP_VELOCITY = -280.0
+const LOW_JUMP = 0.6
 
 
 func _physics_process(delta: float) -> void:
@@ -12,8 +13,10 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y = JUMP_VELOCITY
+	if velocity.y < 0 and Input.is_action_just_released("jump"):
+		velocity.y *= LOW_JUMP
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -29,7 +32,7 @@ func _physics_process(delta: float) -> void:
 			animated_sprite.play("idle")
 		else: 
 			animated_sprite.play("run")
-		
+
 	else: 
 		animated_sprite.play("jump")
 	
