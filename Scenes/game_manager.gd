e5a69869c95de9b25189da64806d3eaa15788500
MayhemK <mayhemk57@gmail.com>
2025-11-05 21:27:47 -0700

    added text with coin count that updates

--- Scenes/game_manager.gd
+++ Scenes/game_manager.gd
@@ -1,7 +1,9 @@
 extends Node
 
 var score = 0
+@onready var label_2: Label = $"../Labels/Label2"
 
 func add_point():
 	score += 1
+	label_2.text = "You collected " + str(score) + "/20 coins"
 	print(score)
--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-@onready var game_manager: Node = $"../../GameManager"
+@onready var game_manager: Node = %GameManager
 
 
 
