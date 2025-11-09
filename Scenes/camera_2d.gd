8afb501277bf665256065e9716dda65bb263e1df
MayhemK <mayhemk57@gmail.com>
2025-11-09 14:11:50 -0700

    working on camera snapping

--- Scenes/camera_2d.gd
+++ Scenes/camera_2d.gd
@@ -1,9 +1,9 @@
 extends Camera2D
  
-func set_camera_limits(new_limit_left, new_limit_top, new_limit_right, new_limit_bottom):
-	set_limit(SIDE_LEFT, 74)
-	set_limit(SIDE_TOP, 0)
-	set_limit(SIDE_RIGHT, 145)
-	set_limit(SIDE_BOTTOM, 12)
-
-	force_update_scroll()
+func set_camera_limits(new_limit_left: int, new_limit_top: int, new_limit_right: int, new_limit_bottom: int):
+	limit_left = new_limit_left
+	limit_top = new_limit_top
+	limit_right = new_limit_right
+	limit_bottom = new_limit_bottom
+	global_position = global_position 
+	
--- scripts/teleport.gd
+++ scripts/teleport.gd
@@ -1,5 +1,9 @@
 extends Area2D
+@onready var player: CharacterBody2D = $"../../player"
+@onready var camera_2d: Camera2D = $"../../player/Camera2D"
 
 
 func _on_body_entered(body):
-	print("you have finished the level")
+	if body == player:
+		print("you have finished the level")
+		camera_2d.set_camera_limits(1185, 0, 145, 206)
