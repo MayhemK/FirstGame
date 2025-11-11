c3e292902a7f9ad75aad37b65b9c60fef6cc8f2f
MayhemK <mayhemk57@gmail.com>
2025-11-10 22:53:25 -0700

    adding a slow effect between levels. need to maybe pan or filter camera between levels?

--- scripts/teleport.gd
+++ scripts/teleport.gd
@@ -1,10 +1,16 @@
 extends Area2D
 @onready var player: CharacterBody2D = $"../../player"
 @onready var camera_2d: Camera2D = $"../../player/Camera2D"
-
+@onready var timer: Timer = $Timer
 
 func _on_body_entered(body):
 	if body == player:
 		print("you have finished the level")
 		camera_2d.set_camera_limits(1185, 0, 2336, 208)
 		player.global_position = Vector2(1264, 139)
+		Engine.time_scale = 0.5
+		timer.start()
+
+
+func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
