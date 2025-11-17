203ff99d6aea8cf11eb0e4c335af67f77bbb8663
MayhemK <mayhemk57@gmail.com>
2025-11-16 23:19:08 -0500

    not sure, probably something else is causing it to reset

--- scripts/teleport.gd
+++ scripts/teleport.gd
@@ -3,19 +3,21 @@ extends Area2D
 @onready var camera_2d: Camera2D = $"../../player/Camera2D"
 @onready var timer: Timer = $Timer
 var level = 0
-var xl = 0
-var yt = 0
-var xr = 1150
-var yb = 208
-
 func _on_body_entered(body):
 	if body == player:
-		level += 1
-		xl += 1185
-		xr += 1150
-		
-		camera_2d.set_camera_limits(xl, 0, xr, 208)
-		player.global_position = Vector2((xl + 114), 139)
+		level += 1 
+		if level == 0:
+			camera_2d.set_camera_limits(0, 0, 1150, 208)
+			player.global_position = Vector2(114, 139)
+		if level == 1:
+			camera_2d.set_camera_limits(1185, 0, 2335, 208)
+			player.global_position = Vector2(1264, 139)
+		if level == 2:
+			camera_2d.set_camera_limits(2370, 0, 3520, 208)
+			player.global_position = Vector2(2449, 139)
+		if level == 3:
+			camera_2d.set_camera_limits(3355, 0, 4705, 208)
+			player.global_position = Vector2(3634, 139)
 		Engine.time_scale = 0.5
 		timer.start()
 
