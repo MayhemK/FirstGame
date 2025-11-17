8a81aa65cf276c1221fb93cb52205bb97d25a736
MayhemK <mayhemk57@gmail.com>
2025-11-16 22:45:29 -0500

    worl isnt progressing like I want

--- scripts/teleport.gd
+++ scripts/teleport.gd
@@ -3,13 +3,19 @@ extends Area2D
 @onready var camera_2d: Camera2D = $"../../player/Camera2D"
 @onready var timer: Timer = $Timer
 var level = 0
+var xl = 0
+var yt = 0
+var xr = 1150
+var yb = 208
 
 func _on_body_entered(body):
 	if body == player:
 		level += 1
-		print("you have finished the level")
-		camera_2d.set_camera_limits(1185, 0, 2336, 208)
-		player.global_position = Vector2(1264, 139)
+		xl += 1185
+		xr += 1150
+		
+		camera_2d.set_camera_limits(xl, 0, xr, 208)
+		player.global_position = Vector2((xl + 114), 139)
 		Engine.time_scale = 0.5
 		timer.start()
 
