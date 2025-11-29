2898a72acc861d992adab811672bb3dcc965a43a
MayhemK <mayhemk57@gmail.com>
2025-11-28 19:18:47 -0700

    trying to figure out fad transition

--- main_menu.gd
+++ main_menu.gd
@@ -1,10 +1,15 @@
 extends Control
 
+var button_type = null
+
 func _ready():
 	%Play.pressed.connect(play)
 	%Quit.pressed.connect(quit_game)
 
 func play():
+	$fade.show()
+	$fade/fade_timer.start()
+	$fade/AnimationPlayer.play("fade_in")
 	get_tree().change_scene_to_file('res://Scenes/game.tscn')
 
 func quit_game():
