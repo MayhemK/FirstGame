c0362cc6e86e001cb77e349b194ec6b10505ecc0
MayhemK <mayhemk57@gmail.com>
2025-11-28 18:28:49 -0700

    went back to first setup

--- main_menu.gd
+++ main_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 func _ready():
 	%Play.pressed.connect(play)
-	%Quit.presset.connect(quit_game)
+	%Quit.pressed.connect(quit_game)
 
 func play():
 	get_tree().change_scene_to_file('res://Scenes/game.tscn')
