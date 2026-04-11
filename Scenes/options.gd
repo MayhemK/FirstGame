0c63bdede652bf81202dc19908b2279b84069267
MayhemK <mayhemk57@gmail.com>
2026-04-11 15:03:44 -0600

    options has return to main menu

--- Scenes/options.gd
+++ Scenes/options.gd
@@ -9,3 +9,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func return_main_menu():
+	get_tree().change_scene_to_file('res://Scenes/main_menu.tscn')
