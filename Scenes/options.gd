c4bc7a6c3d72236a2a633d2a2225e6aeb0c739c3
MayhemK <mayhemk57@gmail.com>
2026-05-26 22:30:15 -0600

    trying some help for volume

--- Scenes/options.gd
+++ Scenes/options.gd
@@ -1,14 +1,30 @@
-extends Node2D
+extends HSlider
 
+# Name of the audio bus you want to control (e.g., "Master" or "Music")
+@export var bus_name: String = "Master"
 
-# Called when the node enters the scene tree for the first time.
+var bus_index: int
+
 func _ready() -> void:
-	pass # Replace with function body.
+	# Get the index of the audio bus
+	bus_index = AudioServer.get_bus_index(bus_name)
+	
+	# Optional: Sync the slider's starting value with the current bus volume
+	var current_db = AudioServer.get_bus_volume_db(bus_index)
+	value = db_to_linear(current_db)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+func _on_value_changed(new_value: float) -> void:
+	# Convert the 0.0 - 1.0 slider value to Decibels
+	var db_volume = linear_to_db(new_value)
+	
+	# Apply it to the audio server
+	AudioServer.set_bus_volume_db(bus_index, db_volume)
+	
+	# If the slider is at 0, completely mute the bus to save processing and avoid faint static
+	if new_value == 0:
+		AudioServer.set_bus_mute(bus_index, true)
+	else:
+		AudioServer.set_bus_mute(bus_index, false)
 func return_main_menu():
 	get_tree().change_scene_to_file('res://Scenes/main_menu.tscn')
