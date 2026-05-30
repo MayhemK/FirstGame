7303927babbf94286f70417e3dd71b17eeb7b3a6
Ivan <mayhemk57@gmail.com>
2026-05-30 13:00:15 -0600

    volumes are working i think

--- Scenes/options.gd
+++ Scenes/options.gd
@@ -1,30 +1,35 @@
-extends HSlider
+extends Node2D
 
-# Name of the audio bus you want to control (e.g., "Master" or "Music")
-@export var bus_name: String = "Master"
-
-var bus_index: int
+@export var master_slider: HSlider
+@export var bgm_slider: HSlider
+@export var sfx_slider: HSlider
 
 func _ready() -> void:
-	# Get the index of the audio bus
-	bus_index = AudioServer.get_bus_index(bus_name)
+	setup_slider(master_slider, "Master")
+	setup_slider(bgm_slider, "BGM")
+	setup_slider(sfx_slider, "SFX")
+
+
+func setup_slider(slider: HSlider, bus_name: String) -> void:
+	if not slider: 
+		print("Warning: Slider for ", bus_name, " is not assigned in the Inspector!")
+		return
 	
-	# Optional: Sync the slider's starting value with the current bus volume
+	var bus_index = AudioServer.get_bus_index(bus_name)
+	
 	var current_db = AudioServer.get_bus_volume_db(bus_index)
-	value = db_to_linear(current_db)
+	slider.value = db_to_linear(current_db)
+	
+	slider.value_changed.connect(func(new_value: float):
+		var db_volume = linear_to_db(new_value)
+		AudioServer.set_bus_volume_db(bus_index, db_volume)
+		
+		if new_value == 0:
+			AudioServer.set_bus_mute(bus_index, true)
+		else:
+			AudioServer.set_bus_mute(bus_index, false)
+	)
 
 
-func _on_value_changed(new_value: float) -> void:
-	# Convert the 0.0 - 1.0 slider value to Decibels
-	var db_volume = linear_to_db(new_value)
-	
-	# Apply it to the audio server
-	AudioServer.set_bus_volume_db(bus_index, db_volume)
-	
-	# If the slider is at 0, completely mute the bus to save processing and avoid faint static
-	if new_value == 0:
-		AudioServer.set_bus_mute(bus_index, true)
-	else:
-		AudioServer.set_bus_mute(bus_index, false)
 func return_main_menu():
 	get_tree().change_scene_to_file('res://Scenes/main_menu.tscn')
